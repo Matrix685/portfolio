21d2a6a6b33ce32ee7333fdce6edbfd5598ef0a1
Matrix685 <oflynnc48len@gmail.com>
2023-11-29 19:47:57 +0000

    made better

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 const buttons = document.getElementsByClassName("buttons"),
-    articles = document.getElementsByTagName("article"),
-    toolbar = document.getElementById("toolbar");
+  articles = document.getElementsByTagName("article"),
+  toolbar = document.getElementById("toolbar");
 
 for (const button of buttons) {
-    button.onclick = () => {
-        var activeBtn = document.querySelector("p.active-btn");
+  button.onclick = () => {
+    var activeBtn = document.querySelector("p.active-btn");
 
-        articleIndex(button.id);
+    articleIndex(button.id);
 
-        activeBtn.classList.remove("active-btn");
-        button.classList.add("active-btn");
+    activeBtn.classList.remove("active-btn");
+    button.classList.add("active-btn");
 
-        activeBtn = button;
-    };
+    activeBtn = button;
+  };
 }
 
 function articleIndex(index) {
-    var activeArticle = document.querySelector('article[data-status="active"]');
+  var activeArticle = document.querySelector('article[data-status="active"]');
 
-    activeArticle.dataset.status = "inactive";
+  activeArticle.dataset.status = "inactive";
 
-    for (const article of articles) {
-        if (article.dataset.index == index) {
-            article.dataset.status = "active";
+  for (const article of articles) {
+    if (article.dataset.index == index) {
+      article.dataset.status = "active";
 
-            activeArticle = article;
-        }
+      activeArticle = article;
     }
+  }
 }
 
 window.onscroll = () => {
-    const scrollHeight = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollTop;
 
-    if (scrollHeight > 30) {
-        toolbar.classList.add("scrolled");
-    }
+  if (scrollHeight > 30) {
+    toolbar.classList.add("scrolled");
+  }
 
-    if (scrollHeight < 30) {
-        toolbar.classList.remove("scrolled");
-    }
+  if (scrollHeight < 30) {
+    toolbar.classList.remove("scrolled");
+  }
 
-    const header = document.getElementById("header"),
-        img = document.getElementById("img"),
-        percent = (scrollHeight / header.clientHeight) * -25 + 30;
+  const header = document.getElementById("header"),
+    img = document.getElementById("img"),
+    percent = (scrollHeight / header.clientHeight) * -25 + 30;
 
-    img.style.backgroundPosition = `center ${percent}%`;
+  img.style.backgroundPosition = `center ${percent}%`;
 };
