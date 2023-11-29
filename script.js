064ddc8190f37a65463f558dad1958f4661d5ec3
Matrix685 <oflynnc48len@gmail.com>
2023-11-29 19:54:20 +0000

    removed

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-const buttons = document.getElementsByClassName("buttons"),
-  articles = document.getElementsByTagName("article"),
-  toolbar = document.getElementById("toolbar");
-
-for (const button of buttons) {
-  button.onclick = () => {
-    var activeBtn = document.querySelector("p.active-btn");
-
-    articleIndex(button.id);
-
-    activeBtn.classList.remove("active-btn");
-    button.classList.add("active-btn");
-
-    activeBtn = button;
-  };
-}
-
-function articleIndex(index) {
-  var activeArticle = document.querySelector('article[data-status="active"]');
-
-  activeArticle.dataset.status = "inactive";
-
-  for (const article of articles) {
-    if (article.dataset.index == index) {
-      article.dataset.status = "active";
-
-      activeArticle = article;
-    }
-  }
-}
+const toolbar = document.getElementById("toolbar");
 
 window.onscroll = () => {
   const scrollHeight = document.documentElement.scrollTop;
