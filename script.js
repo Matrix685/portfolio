894ac90bd141a95299c8ba7bd26dc1b8c9917104
Matrix685 <oflynnc48len@gmail.com>
2024-05-13 14:06:33 +0100

    changed stuff

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const toolbar = document.getElementById("toolbar");
 
 window.onscroll = () => {
-  const scrollHeight = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollTop;
 
-  if (scrollHeight > 30) {
-    toolbar.classList.add("scrolled");
-  }
+    if (scrollHeight > 30) {
+        toolbar.classList.add("scrolled");
+    }
 
-  if (scrollHeight < 30) {
-    toolbar.classList.remove("scrolled");
-  }
+    if (scrollHeight < 30) {
+        toolbar.classList.remove("scrolled");
+    }
 
-  const header = document.getElementById("header"),
-    img = document.getElementById("img"),
-    percent = (scrollHeight / header.clientHeight) * -25 + 30;
+    const header = document.getElementById("header"),
+        img = document.getElementById("img"),
+        percent = (scrollHeight / header.clientHeight) * -25 + 30;
 
-  img.style.backgroundPosition = `center ${percent}%`;
+    img.style.backgroundPosition = `center ${percent}%`;
 };
